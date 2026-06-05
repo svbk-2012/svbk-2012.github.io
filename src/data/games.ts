4b404adc31b690580eaa9b2e58d7b4f23a12fc5e
svbk-2012 <154873908+svbk-2012@users.noreply.github.com>
2026-06-05 17:22:39 -0400

    Create games.ts

---
+++ src/data/games.ts
@@ -0,0 +1,427 @@
+import { Game } from '../types';
+
+export const games: Game[] = [
+  // ── CHAT / AI ──
+  {
+    title: 'City Skyline copy',
+    description: 'I used ai to make some of the game.',
+    url: 'https://tharaniidaran-t.github.io/cityskylinesremake/',
+    thumbnail: 'https://upload.wikimedia.org/wikipedia/en/5/58/Cities_Skylines_cover_art.jpg',
+    category: 'Fun'
+  },
+  {
+    title: 'Chatgpt',
+    description: 'I used ai to make some of the game.',
+    url: 'https://chatgpt.org/',
+    thumbnail: 'https://upload.wikimedia.org/wikipedia/en/5/58/Cities_Skylines_cover_art.jpg',
+    category: 'Fun'
+  },
+  {
+    title: 'The Final Earth 2',
+    description: 'I love this game bro. Actually best game out there, please try atleast once!',
+    url: 'clfinalearth2.html',
+    thumbnail: 'https://i.ytimg.com/vi/9lZ9dCxejp0/hqdefault.jpg',
+    category: 'BEST GAME OUT THERE'
+  },
+  {
+    title: 'Shader Pilot',
+    description: 'Game that TTM made and It looks good Ig and cool ig. ',
+    url: 'https://tharaniidaran-t.github.io/shader-piolet/',
+    thumbnail: 'https://i.ytimg.com/vi/cpsY9WQxh5U/maxresdefault.jpg',
+    category: 'Game'
+  },
+  {
+    title: 'unblocked anime stuff',
+    description: '',
+    url: 'https://streamex.sh/s',
+    thumbnail: 'https://media.licdn.com/dms/image/v2/C4D0BAQHfLNZeLxAnSw/company-logo_200_200/company-logo_200_200/0/1660365825078/streamx_logo?e=2147483647&v=beta&t=pgzHh-mNGlpjcTsC6qxYkW1kRSaOzk0MZlOCL46CmQ0',
+    category: 'Game'
+  },
+  {
+    title: 'Chatgpt (GROQ API)',
+    description: 'Go to the doc and get a API or swap your model if it says max token limit reached. ',
+    url: 'https://tharaniidaran-t.github.io/GroqAI2/',
+    thumbnail: 'https://www.internetmatters.org/wp-content/uploads/2025/06/Chat-GPT-logo.webp',
+    category: 'AI'
+  },
+  {
+    title: 'Youtube',
+    description: 'Youtube unblocked, should allow you to paste videos into the URL thingy and watch it.',
+    url: 'https://granddia2.github.io/old/yt.html',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_yL7AtgrIozF4QESx27DN6CDTvAuxtEfPPw&s',
+    category: 'Solo'
+  },  
+  {
+    title: 'Proxy',
+    description: 'Allows you to scour the web unblocked',
+    url: 'https://somany.gsxpress.com.my/',
+    thumbnail: 'https://cdn.prod.website-files.com/65e50602c9dc544319635b46/65e88c2b238ceb5fe132c177_proxy-logo.png',
+    category: 'Solo'
+  },
+  // ── SOLO ──
+  {
+    title: 'Slope',
+    description: 'This is just for the girls ig',
+    url: 'https://ubg365.github.io/slope/play.html',
+    thumbnail: 'https://i.ytimg.com/vi/jvjZ4rnERmM/maxresdefault.jpg',
+    category: 'Solo'
+  },
+  {
+    title: 'Epilepsy game',
+    description: 'This is just for the girls ig',
+    url: 'https://ubg365.github.io/death-run-3d/',
+    thumbnail: 'https://play-lh.googleusercontent.com/IeGUGo20PsUgjtAajcZWsz09ypyAIITXS8jDzzLAFdoR65Zy_3PVOP-ckq5b_A_vPzuV=w526-h296-rw',
+    category: 'Solo'
+  },
+  {
+    title: '2048',
+    description: 'Girls.',
+    url: 'https://ubg365.github.io/2048/play.html',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSuxqLfXqm07uXPH0qyPu_6MofiU9nlUoq9w&s',
+    category: 'Solo'
+  },
+  {
+    title: 'Bitlife',
+    description: 'Most fun to experiment by yourself.',
+    url: 'https://ubg365.github.io/bitlife-life-simulator/play.html',
+    thumbnail: 'https://i.ytimg.com/vi/WUTrv--kZtI/maxresdefault.jpg',
+    category: 'Single-Player'
+  },
+  {
+    title: 'OvO',
+    description: 'Fun to play by yourself.',
+    url: 'https://mc0825.github.io/g5/class-456',
+    thumbnail: 'https://play-lh.googleusercontent.com/v7KwGdPjJGjJjRXn46sck4DwDBdKSeRzGN44CjiXUtKV0jjOi51Bt4wcXud0m-SkXg',
+    category: 'Platformer'
+  },
+  {
+    title: 'Wordle Unlimited',
+    description: '',
+    url: 'https://bosorioo.github.io/wordle-unlimited/',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzXo17pL3S9PjcsR4V-ZPUr8nuQ622Lq0lhA&s',
+    category: 'Skill'
+  },
+  {
+    title: 'Sandboxels',
+    description: 'works now I think',
+    url: 'https://mr-funkinguy.github.io/sandboxels/',
+    thumbnail: 'https://s3.amazonaws.com/production2.sciencegamecenter.org/games/boxarts/000/000/417/medium/TitleArt_SandBoxels.jpeg?1707496550',
+    category: 'Science'
+  },
+  {
+    title: 'Tanuki Sunset',
+    description: '',
+    url: 'https://mc0825.github.io/g26/class-488/',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBc_knTaEJfOEGjJPNFjIsNG5Mk95b6ua-vw&s',
+    category: 'Driving'
+  },
+  {
+    title: 'Minecraft Launcher',
+    description: 'Fun to play with your friends and by yourself.',
+    url: 'https://irv77.github.io/AmplerLauncher/',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3i6T2Z3GQtOp-O6IC1qtggAoP3_-vm0RsPw&s',
+    category: 'Creative'
+  },
+  {
+    title: 'Bloons TD6 copy',
+    description: 'A knockoff',
+    url: 'https://script.google.com/macros/s/AKfycbzBb8Muvm--ZUnA0UYEGuaQzw-HyM8ahDYvCRGPsufNMfIrpdLmSJ9i8EtkRpd3hvwI/exec',
+    thumbnail: 'bloons.jpg',
+    category: 'Solo or Multiplayer.'
+  },
+  {
+    title: 'Fnaf',
+    description: 'Survive.',
+    url: 'https://ubg365.github.io/fnaf/play.html',
+    thumbnail: 'https://static.wikia.nocookie.net/freddy-fazbears-pizza/images/d/da/FNaF1.jpg/revision/latest/scale-to-width-down/1200?cb=20230410020858',
+    category: 'Solo'
+  },
+  {
+    title: 'Fnaf 2',
+    description: 'Survive.',
+    url: 'https://ubg365.github.io/fnaf-2/',
+    thumbnail: 'https://m.media-amazon.com/images/I/81HBpJd2h8L.png',
+    category: 'Solo'
+  },
+  {
+    title: 'Fnaf 3',
+    description: 'Survive.',
+    url: 'https://ubg365.github.io/fnaf-3/',
+    thumbnail: 'https://static.wikia.nocookie.net/freddy-fazbears-pizza/images/f/fd/FNaF_3_Switch.jpg/revision/latest?cb=20210405111619',
+    category: 'Solo'
+  },
+  {
+    title: 'Tetris',
+    description: 'Fun.',
+    url: 'https://ubg365.github.io/flash-tetris/',
+    thumbnail: 'https://www.datocms-assets.com/145957/1744284280-tetris-mobile.png?auto=format&fit=max&w=1200',
+    category: 'Solo'
+  },
+  {
+    title: 'Tomb of the Mask',
+    description: 'Fun to play',
+    url: 'https://doraedu52.github.io/g26/class-438',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNHRR0Dx3LhBe3OA0pHR0U_VuPagdWjz1pOg&s',
+    category: 'solo' 
+  },
+  {
+    title: 'Crossy Road',
+    description: '2nd one if first does not work. 1st one if 2nd one does not work',
+    url: 'https://crossy-road-unblock.github.io/crossy-road/',
+    thumbnail: 'https://crossyroadonline.bitbucket.io/img/crossy-road.png',
+    category: 'solo'
+  },
+  {
+    title: 'Champion Island',
+    description: 'Google Doodle',
+    url: 'https://granddia2.github.io/championisland/',
+    thumbnail: 'https://www.gstatic.com/marketing-cms/c2/de/b1ea4f8d4314be89cbd66a6c94d7/4.gif',
+    category: 'solo'
+  },
+  {
+    title: 'FIFA 11',
+    description: 'yeah',
+    url: '/clFIFA11.html',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpOYk7hDNvaL46ncyl7PbEdtncc_EaXvXsCw&s',
+    category: 'solo'
+  },
+  {
+    title: 'Madden 2002',
+    description: 'yeah',
+    url: '/clmaddennfl2002.html',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNXrI6cQ7sBJyZenq-Ur4Cz3V8ZQHgWpFf4XEnWDQgkiSMjE37CiA0g7Ew7Lz1dp78kCcWRRIPo179hdLl4G8XJmFxmwSaZ2Gpt97hkw&s=10',
+    category: 'solo'
+  },
+  {
+    title: 'Call of Duty 4',
+    description: 'yeah',
+    url: '/clcod4.html',
+    thumbnail: 'https://m.media-amazon.com/images/I/61bs4F5yOIL._AC_UF1000,1000_QL80_.jpg',
+    category: 'solo'
+  },
+  {
+    title: 'GTA Advance',
+    description: 'yeah',
+    url: '/clgrandtheftautoadvance.html',
+    thumbnail: 'https://m.media-amazon.com/images/M/MV5BNjRhYzk1ODItYjFhNy00OGU4LWE2YjAtZTk3NmRlNDhiZTFjXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+    category: 'solo'
+  },
+  {
+    title: 'NFS: Most Wanted',
+    description: 'yeah',
+    url: '/clnfsmostwanted.html',
+    thumbnail: 'https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1262560/capsule_616x353.jpg?t=1777484382',
+    category: 'solo'
+  },
+  {
+    title: 'Suika Watermelon copy',
+    description: 'yeah',
+    url: 'https://tharaniidaran-t.github.io/suika-game/',
+    thumbnail: 'https://media.tenor.com/7vR97sAI99IAAAAe/watermelon-game-suika-game.png',
+    category: 'solo'
+  },
+  {
+    title: 'Pokemon Platinum',
+    description: 'yeah',
+    url: '/clpokeplatinum.html',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt28qRa0-4kRzGsgiZ76XHYAhuNG6jYlt-Ud40owAzpJrXl4dHHRDHcavTsyQYjsBmvXu1Sw&s=10',
+    category: 'solo'
+  },
+  {
+    title: 'Pokemon Black 2',
+    description: 'yeah',
+    url: '/clpokeblack2.html',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuOhDpOLhmmSUkCuI-aDpmhwF7iygOrqUqeg&s',
+    category: 'solo'
+  },
+  {
+    title: 'Pokemon SoulSilver',
+    description: 'yeah',
+    url: '/clpokesoulsilver.html',
+    thumbnail: 'https://media.gamestop.com/i/gamestop/10077723/Pokemon-SoulSilver-Game-Only---Nintendo-DS?w=768&h=768&fmt=auto',
+    category: 'solo'
+  },
+   {
+    title: 'Tomodachi Collection',
+    description: 'yeah',
+    url: '/cltomodachicollection.html',
+    thumbnail: 'https://i.ytimg.com/vi/H96rM_liOHo/maxresdefault.jpg',
+    category: 'solo'
+  },
+  {
+    title: 'Crazy Cattle 3D',
+    description: 'yeah',
+    url: '/clcrazycattle3d.html',
+    thumbnail: 'https://crazycattle3d.github.io/index.png',
+    category: 'solo'
+  },
+  // ── MULTIPLAYER ──
+  {
+    title: 'Animals Volleyball',
+    description: 'Probably the most fun game, also can play with friends.',
+    url: 'https://mc0825.github.io/g66/class-847/',
+    thumbnail: 'https://img.poki-cdn.com/cdn-cgi/image/q=78,scq=50,width=1200,height=1200,fit=cover,f=png/966cec3b19cedf7dd72f93419f13268c/animals-volleyball-logo.png',
+    category: 'Sports'
+  },
+  {
+    title: 'Tag',
+    description: 'Fun to play with your friends.',
+    url: 'https://mc0825.github.io/g69/class-633/',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqnv5dEDOurGtf2ftRLZDASTNMHsTCh8eDhg&s',
+    category: 'Multiplayer'
+  },
+  {
+    title: 'Basket Random',
+    description: '',
+    url: 'https://mc0825.github.io/g26/class-436/',
+    thumbnail: 'https://images.sftcdn.net/images/t_app-cover-s-16-9,f_auto/p/c980f465-a1b3-49d0-a53a-bca3d956be49/2213180539/basket-random-unblocked-screenshot',
+    category: 'Platformer'
+  },
+  {
+    title: 'Smash Karts',
+    description: '',
+    url: 'https://script.google.com/macros/s/AKfycbyE1C_kxeomVoOLjpaXc2uAI7JdNgVhdQdePTZu9Sizwzj_imhaWnNmPSXm4w1v-SFs/exec',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZeAncUPzcpBXnoPduVqHD_P5gfghYmA6rKw&s',
+    category: 'Shooter'
+  },
+  {
+    title: 'Leader Strike',
+    description: '',
+    url: 'https://inkyedu118.github.io/g97/class-542',
+    thumbnail: 'https://i.ytimg.com/vi/rIqaCUaAY1k/mqdefault.jpg',
+    category: 'Shooter'
+  },
+  {
+    title: 'Imposter',
+    description: '',
+    url: 'https://script.google.com/macros/s/AKfycbxcouwzwIrULxhM_6KgC3GOg7xZ7C0A3s4weEUQEtWdwb08Ma3PhJuLZVONJXKJG9DKaA/exec',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqnHES0vMhKA4ZN9RoVVmLKs__68g8gOUZOw&s',
+    category: 'Party'
+  },
+  {
+    title: 'Volley Random',
+    description: 'Fun to play with your friends.',
+    url: 'https://mc0825.github.io/g/class-811',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE-WsAiFSXWoWlHvWGYm4xNQysU_kK39VvcRz54bY9a1mdBapeDJne6gevddjqVG_5qhvQZIfZx3POJnNa6sJ-fBbEVW-oUY7-Rv27LwQ&s=10',
+    category: 'Fast Paced'
+  },
+  {
+    title: 'Among Us',
+    description: '',
+    url: 'https://script.google.com/macros/s/AKfycbwdYUtgs7kZjgJ5OG0WpNqc84LYICfZo0ezR1WfAYdjwqfkasZmB0Ad4MPFNVBfyKH-/exec',
+    thumbnail: 'https://i.ytimg.com/vi/0YKjFoGxbec/maxresdefault.jpg',
+    category: 'Party'
+  },
+  {
+    title: 'Wavelength',
+    description: 'Fun to play with your friends.',
+    url: 'https://script.google.com/macros/s/AKfycbwq4d8v_dkz0sx64ODo_5y7FMAboBGGRLfoAk-Z_KwcOT1JGC7RO1EWHQh7b4xipEwn/exec',
+    thumbnail: 'https://www.mathsisfun.com/algebra/images/wavelength-amplitude.svg',
+    category: 'Party'
+  },
+  {
+    title: 'Chess',
+    description: 'Play chess with a friend online using a room code.',
+    url: '/chess.html',
+    thumbnail: 'https://images.chesscomfiles.com/uploads/v1/blog/291978.333e4ddb.630x354o.6e5e89b5223a.png',
+    category: 'Multiplayer'
+  },
+  {
+    title: 'Wordle 1v1',
+    description: 'Wordle but multiplayer',
+    url: 'https://script.google.com/macros/s/AKfycbxgCfdpeZZi6jP7SOZcS3ZtBDRRRNEQX1ENPColTVrm2VHBcNBpKeOs87ddHC0xwr7_-A/exec',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzXo17pL3S9PjcsR4V-ZPUr8nuQ622Lq0lhA&s',
+    category: 'Multiplayer'
+  },
+  {
+    title: 'Mafia',
+    description: 'Game where an informed minority (the Mafia) against an uninformed majority (the Villagers).',
+    url: 'https://script.google.com/macros/s/AKfycbxIJpXSuYYb5fem5sk7ihiqcfGBn3n_3AiqHzWyA5ivVxhI2-Zzk5kGmJM3ym5cLYmM/exec',
+    thumbnail: 'https://icebreakerideas.com/wp-content/uploads/2019/04/Mafia-Game-e1610793771411.jpg',
+    category: 'Party'
+  },
+  {
+    title: 'Rocket goal',
+    description: 'Fun to play with your friends.',
+    url: 'https://script.google.com/macros/s/AKfycbybZkVPGyvHUOZbwrJSn5fRQIFcGpoIEhp0r-yq2GWRtE_1G7YzP4t8kPZRfp6tutUN/exec',
+    thumbnail: 'https://play-lh.googleusercontent.com/VFwCWELna7i6okl299W0e1H-0moEvVfT9N2M9moaikhCTcEDjUg3hE1mkSlm3ZezfLPi4ppMcStIhjWUustesg=w526-h296-rw',
+    category: 'Puzzle'
+  },
+  {
+    title: 'Wrassling',
+    description: 'Fun to play with your friends.',
+    url: 'https://mc0825.github.io/g69/class-651',
+    thumbnail: 'https://img.poki-cdn.com/cdn-cgi/image/q=78,scq=50,width=1200,height=1200,fit=cover,f=png/f1b3c828-7af9-4e13-9f5c-b2e88f335bdf/wrassling.png',
+    category: 'Multiplayer'
+  },
+  {
+    title: 'Soccer Random',
+    description: 'Fun to play with your friends.',
+    url: 'https://mc0825.github.io/g26/class-511',
+    thumbnail: 'https://play-lh.googleusercontent.com/G1PIlb6HWKSaDre0XpUcmKGps9T4iamsSlwrogB3EJzYv4bz0M2am4D17MtGzndaOOU=w240-h480-rw',
+    category: 'Fast Paced'
+  },
+  {
+    title: 'Getaway Shootout',
+    description: 'Fun to play with your friend.',
+    url: 'https://mc0825.github.io/g9/class-479/',
+    thumbnail: 'https://getawayshootoutonline.github.io/images/getaway-shootout.png',
+    category: 'Multiplayer'
+  },
+  {
+    title: 'Skribbl.io',
+    description: 'Fun to play with your friend.',
+    url: 'https://skribbl.io/',
+    thumbnail: 'https://static.wikitide.net/partycrasherswiki/1/14/IMPROVED-Skribblio.png',
+    category: 'Multiplayer'
+  },
+  {
+    title: 'Uno',
+    description: 'Make sure to say Uno',
+    url: 'https://script.google.com/macros/s/AKfycbyOGsDEwmKxazJAHOb9fXbJFZM0LwmZJPkmKdLKa1qPY6OWfKIP9GcqbnkRf0JbUWmERQ/exec',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEhekk0qTgz3FPvdI-7B2BajmVa-AQgjwkQA&s',
+    category: 'Multiplayer'
+  },
+  {
+    title: 'Gartic Phone Copy',
+    description: '',
+    url: 'https://tharaniidaran-t.github.io/gartic-phone.github/',
+    thumbnail: 'https://static.wikia.nocookie.net/gartic-phone/images/8/87/Garticphone.png/revision/latest/scale-to-width-down/250?cb=20230104102856',
+    category: 'Multiplayer'
+  },
+  {
+    title: 'Block Blast',
+    description: '',
+    url: 'clblockblastv2.html',
+    thumbnail: 'https://play-lh.googleusercontent.com/R0qgNDYYHbRhw6JFsdEbDMqONplEvJx0m0W9wzYVvY3eNF1c2rfBWYjQxW0sLEzFe1E=w240-h480-rw',
+    category: 'Solo'
+  },
+  {
+    title: 'yandere Simulator',
+    description: 'totally requested by some1...',
+    url: 'clyanderesimulator.html',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEJtZ7Fkd1QUQOtTNVBsHB2992ICRvmqHIGHUBOIZAqC1BAfat7lwcuqq-CKaq9uhCF21i&s=10',
+    category: 'Solo'
+  },
+  {
+    title: 'Ace Attorney',
+    description: '...',
+    url: 'aceattorney.html',
+    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_YlHon89nCvWBzs71pzFmcJWQ4O7zmvfo07Zv85mpQddjo0kLlNWmLEIRqMj1o3BSTy0zsWyLEgrjQUt8ssfh-nkPFBxhKcM216lujQ&s=10',
+    category: 'Solo'
+  },
+  {
+    title: '20 Questions',
+    description: '...',
+    url: 'https://script.google.com/macros/s/AKfycbyUrnTBOG7GX18cuwZyso8g60AeCiUJ0z0jaMFtbj1ef8NnxhjMKGSUyANQnEHIhEYj9Q/exec',
+    thumbnail: 'https://m.media-amazon.com/images/I/61mPeDyQewL._AC_UF894,1000_QL80_.jpg',
+    category: 'Multiplayer'
+  },
+  {
+    title: 'Hangman',
+    description: '...',
+    url: 'https://script.google.com/macros/s/AKfycbwZ5rAmhbYeKMGab21sLjLKVpxYtSFEYesWc5v0R2Ya1zjsXMO6_9OzZGGqkFISiRZf/exec',
+    thumbnail: 'https://t4.ftcdn.net/jpg/14/80/76/33/360_F_1480763300_sQsVKV59fW7BEKo60WOVVPqlFDen30WS.jpg',
+    category: 'Multiplayer'
+  }
+];
